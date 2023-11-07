98f873028dfd0da84d8734eb1d1fc758cd53496a
Lizzie <lizziejacoboo@gmail.com>
2023-11-07 16:16:57 -0700

    created base html and css for quiz, added 5 questions and answers to javascript

---
+++ assets/script.js
@@ -0,0 +1,46 @@
+const questions = [ {
+  question: "How do you write a single-line comment in JavaScript?",
+  answers: [
+    { text: "//Comment", correct: true},
+    { text: "<-Comment->", correct: false},
+    { text: "/*Comment*/", correct: false},
+    { text: "#Comment", correct: false}
+    ]
+  },
+  {
+    question: "What does the NaN value represent in JavaScript?",
+    answers: [
+      { text: "No available Number", correct: false},
+      { text: "No additional Name", correct: false},
+      { text: "Not a Number", correct: true},
+      { text: "Not a Newcomer", correct: false}
+      ] 
+  },
+  {
+    question: "What is the purpose of the return statement in a function in JavaScript?",
+    answers: [
+      { text: "To start a loop", correct: false},
+      { text: "To comment out code", correct: false},
+      { text: "To declare a variable", correct: false},
+      { text: "To end the function and return a value to the calling code", correct: true}
+      ] 
+  },
+  {
+    question: "How do you write a conditional statment in JavaScript that checks if a variable x is equal to 5?",
+    answers: [
+      { text: "If x equals 5", correct: false},
+      { text: "If (x == 5)", correct: false},
+      { text: "If (x === 5)", correct: true},
+      { text: "If (x = 5)", correct: false}
+      ] 
+  },
+  {
+    question: "What does the JavaScript typeof operator return wwhen applied to 'true'?",
+    answers: [
+      { text: "Boolean", correct: true},
+      { text: "Array", correct: false},
+      { text: "String", correct: false},
+      { text: "Object", correct: false}
+      ] 
+  },
+];
